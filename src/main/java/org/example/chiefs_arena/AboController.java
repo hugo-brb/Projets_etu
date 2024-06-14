f09ceba206e16c521e5a12dae6720b54f27f26b7
barbiehu <hugo.barbieri@etu.univ-grenoble-alpes.fr>
2024-06-14 11:33:39 +0200

    Changes by lenoimat

---
+++ src/main/java/org/example/chiefs_arena/AboController.java
@@ -0,0 +1,5 @@
+package org.example.chiefs_arena;
+
+public class AboController {
+
+}
--- src/main/java/org/example/chiefs_arena/CreateController.java
+++ src/main/java/org/example/chiefs_arena/CreateController.java
@@ -1,4 +1,42 @@
 package org.example.chiefs_arena;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
 
 public class CreateController {
+    @FXML
+    private TextField nomField;
+    @FXML
+    private TextArea descriptionField;
+    @FXML
+    private ComboBox<String> categorieComboBox;
+    @FXML
+    private ComboBox<String> lieuComboBox;
+    @FXML
+    private TextField lieuSearchField;
+    @FXML
+    private ImageView mapImageView;
+
+    @FXML
+    private void initialize() {
+        // Initialize combo boxes, load map image, etc.
+    }
+
+    @FXML
+    private void handleAjouterCuisinier() {
+        // Logic to add a cook
+    }
+
+    @FXML
+    private void handleAjouterJury() {
+        // Logic to add a jury member
+    }
+
+    @FXML
+    private void handleAjouterSponsor() {
+        // Logic to add a sponsor
+    }
 }
