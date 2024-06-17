20ee8479e72f4c95af1bc4153dcd0e84b0e14eae
barbiehu <hugo.barbieri@etu.univ-grenoble-alpes.fr>
2024-06-17 09:46:53 +0200

    jlk

--- src/main/java/org/example/chiefs_arena/ChiefsArenaApplication.java
+++ src/main/java/org/example/chiefs_arena/ChiefsArenaApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class ChiefsArenaApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ChiefsArenaApplication.class.getResource("create-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ChiefsArenaApplication.class.getResource("home-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Chief's Arena");
         stage.setScene(scene);
