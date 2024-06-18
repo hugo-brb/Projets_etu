1b37123d12bda51328669125294af1d359015871
barbiehu <hugo.barbieri@etu.univ-grenoble-alpes.fr>
2024-06-18 10:03:53 +0200

    Update

--- src/main/java/org/example/chiefs_arena/AppController.java
+++ src/main/java/org/example/chiefs_arena/AppController.java
@@ -1,8 +1,29 @@
 package org.example.chiefs_arena;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import org.w3c.dom.events.MouseEvent;
+
+import java.io.IOException;
 
 public class AppController {
+    @FXML
+    public HBox btnCreate;
 
+    @FXML
+    public void actionCreateEvent(javafx.scene.input.MouseEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("create-view.fxml"));
+        fxmlLoader.setController(this);
+
+        Stage fenetre = (Stage) btnCreate.getScene().getWindow();
+        Scene scene = new Scene(fxmlLoader.load(), 843, 800);
+
+        fenetre.setScene(scene);
+        fenetre.show();
+    }
 }
--- src/main/java/org/example/chiefs_arena/ChiefsArenaApplication.java
+++ src/main/java/org/example/chiefs_arena/ChiefsArenaApplication.java
@@ -3,6 +3,7 @@ package org.example.chiefs_arena;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,10 +12,12 @@ public class ChiefsArenaApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ChiefsArenaApplication.class.getResource("home-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 843, 800);
         stage.setTitle("Chief's Arena");
+        //stage.getIcons().add(new Image("/org/example/chiefs_arena/img/Logo_ChiefsArena_transparentBlack.png"));
+
         stage.setScene(scene);
-        stage.setMaximized(true);
+        stage.setResizable(false);
         stage.show();
     }
 
