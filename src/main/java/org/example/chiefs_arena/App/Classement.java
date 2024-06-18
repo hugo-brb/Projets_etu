66553b8be08aa99f1065069a5acd43d4906761c0
Matheo <math38.lenoir@gmail.com>
2024-06-18 10:38:26 +0200

    update

---
+++ src/main/java/org/example/chiefs_arena/App/Classement.java
@@ -0,0 +1,47 @@
+package org.example.chiefs_arena.App;
+
+import java.util.List;
+
+/**
+ * Représente le classement des participants dans un concours.
+ */
+public class Classement {
+    private int position;
+    private List<Participant> podium;
+
+    /**
+     * Obtient la position du classement.
+     *
+     * @return La position du classement.
+     */
+    public int getPosition() {
+        return position;
+    }
+
+    /**
+     * Définit la position du classement.
+     *
+     * @param position La nouvelle position du classement.
+     */
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    /**
+     * Obtient la liste des participants sur le podium.
+     *
+     * @return La liste des participants sur le podium.
+     */
+    public List<Participant> getPodium() {
+        return podium;
+    }
+
+    /**
+     * Définit la liste des participants sur le podium.
+     *
+     * @param podium La nouvelle liste des participants sur le podium.
+     */
+    public void setPodium(List<Participant> podium) {
+        this.podium = podium;
+    }
+}
