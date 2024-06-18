123b34b5d84b6941b11cb4f3593c21ac1d9d8bb5
idk <aurique-brio0p@icloud.com>
2024-06-18 11:44:52 +0200

    update default template

--- src/main/java/org/example/chiefs_arena/Controller/LoadHome.java
+++ src/main/java/org/example/chiefs_arena/Controller/LoadHome.java
@@ -1,6 +1,21 @@
 package org.example.chiefs_arena.Controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+
 public class LoadHome
 {
+	@FXML
+	FlowPane root;
 
+	@FXML
+	public void initialize() throws IOException
+	{
+		VBox home = FXMLLoader.load(getClass().getResource("home-view.fxml"));
+		root.getChildren().add(home);
+	}
 }
