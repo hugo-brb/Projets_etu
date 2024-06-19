09722f967483fbbda2d931b0b2d4bb0bab0886c9
Matheo <math38.lenoir@gmail.com>
2024-06-19 10:58:13 +0200

    Creation exception concours

---
+++ src/main/java/org/example/chiefs_arena/exception/ConcoursDateInvalide.java
@@ -0,0 +1,7 @@
+package org.example.chiefs_arena.exception;
+
+public class ConcoursDateInvalide extends Exception {
+    public ConcoursDateInvalide(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/org/example/chiefs_arena/exception/DescriptionTropLongue.java
@@ -0,0 +1,7 @@
+package org.example.chiefs_arena.exception;
+
+public class DescriptionTropLongue extends Exception {
+    public DescriptionTropLongue(String message) {
+        super(message);
+    }
+}
